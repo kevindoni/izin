683d4abcb809f09d1705a1fb3318ce8b0a3e4e40
muhammad amin <92498377+Sangarya@users.noreply.github.com>
2021-10-30 22:00:28 +0800

    Update add-trgo.sh

--- trojan-go/add-trgo.sh
+++ trojan-go/add-trgo.sh
@@ -1,102 +1,48 @@
 #!/bin/bash
-
-grey='\x1b[90m'
-red='\x1b[91m'
-green='\x1b[92m'
-yellow='\x1b[93m'
-blue='\x1b[94m'
-purple='\x1b[95m'
-cyan='\x1b[96m'
-white='\x1b[37m'
-bold='\033[1m'
-off='\x1b[m'
-flag='\x1b[47;41m'
-NC='\x1b[m'
-ISP=$(curl -s ipinfo.io/org | cut -d " " -f 2-10 )
-CITY=$(curl -s ipinfo.io/city )
-COUNTRY=$(curl -s ipinfo.io/country )
-
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
 MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Script By Akbar Maulana"
 clear
-uuid1=$(cat /etc/trojan-go/uuid.txt)
+uuid=$(cat /etc/trojan-go/uuid.txt)
 source /var/lib/premium-script/ipvps.conf
 if [[ "$IP" = "" ]]; then
 domain=$(cat /etc/v2ray/domain)
 else
 domain=$IP
 fi
-trojango="$(cat ~/log-install.txt | grep -i TrojanGO | cut -d: -f2|sed 's/ //g')"
-until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${user_EXIST} && ${user_EXISTS} == '0' ]]; do
-		read -rp "Masukan Key : " keyy
-        read -rp "Remarks : " -e user
-		user_EXIST=$(grep -w $user /etc/trojan-go/akun.conf | wc -l)
-        user_EXISTS=$(grep -w $user /etc/trojan/akun.conf | wc -l)
-		if [[ ${user_EXIST} == "1" ]] || [[ ${user_EXISTS}  == '1' ]]; then
-			echo ""
-			echo "Nama User Sudah Ada, Harap Masukkan Nama Lain!"
-			exit 1
-		fi
-	done
-users="Trojan-GO_$user"
-read -p "Expired (hari) : " masaaktif
-tgl=$(date -d "$masaaktif days" +"%d")
-bln=$(date -d "$masaaktif days" +"%b")
-thn=$(date -d "$masaaktif days" +"%Y")
-expe="$tgl $bln, $thn"
-tgl2=$(date +"%d")
-bln2=$(date +"%b")
-thn2=$(date +"%Y")
-tnggl="$tgl2 $bln2, $thn2"
-sed -i '/"'""$uuid1""'"$/a\,"'""$users""'"' /etc/trojan-go/config.json
-exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
-echo -e "### $user $exp" >> /etc/trojan-go/akun.conf
-systemctl restart trojan-go
-trojangolink="trojan-go://${users}@${domain}:${trojango}/?sni=${domain}&type=ws&host=${domain}&path=/brody&encryption=none#${user}"
-cat > client.json << END
-{
-    "run_type": "client",
-    "local_addr": "127.0.0.1",
-    "local_port": 1080,
-    "remote_addr": "${domain}",
-    "remote_port": "${trojango}",
+trgo="$(cat ~/log-install.txt | grep -w "TrojanGO" | cut -d: -f2|sed 's/ //g')"
+until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${user_EXISTS} == '0' ]]; do
+  read -rp "Password : " -e user
+  user_EXISTS=$(grep -w $user /etc/trojan-go/akun.conf | wc -l)
 
-    "dns": [
-        "1.1.1.1"
-    ],
-    "password": [
-        "${users}"
-    ],
-    "ssl": {
-        "sni": "${domain}"
-    },
-    "websocket": {
-        "enabled": true,
-        "path": "\/brody",
-        "hostname": "${domain}"
-    }
-}
-END
-mv client.json /home/vps/public_html/${user}-IgniterGO.json
+  if [[ ${user_EXISTS} == '1' ]]; then
+   echo ""
+   echo "A client with the specified name was already created, please choose another name."
+   exit 1
+  fi
+ done
+read -p "Expired (Days) : " masaaktif
+sed -i '/"'""$uuid""'"$/a\,"'""$user""'"' /etc/trojan-go/config.json
+exp=date -d "$masaaktif days" +"%Y-%m-%d"
+hariini=date -d "0 days" +"%Y-%m-%d"
+echo -e "### $user $exp" >> /etc/trojan-go/akun.conf
+systemctl restart trojan-go.service
+link="trojan-go://${user}@${domain}:${trgo}/?sni=${domain}&type=ws&host=${domain}&path=/trojango&encryption=none#$user"
 clear
 echo -e ""
-echo -e "${red}=================================${off}"
-echo -e "${white}            TROJAN GO${off}"
-echo -e "${red}=================================${off}"
-echo -e " ${green}ISP                : ${ISP}"
-echo -e " CITY               : ${CITY}"
-echo -e " COUNTRY            : ${COUNTRY}"
-echo -e " Remarks            : ${user}"
-echo -e " Host               : ${domain}"
-echo -e " Port Trojan-GO     : ${trojango}"
-echo -e " Key Trojan-GO    : ${keyy}"
-echo -e " Password Igniter   : ${users}"
-echo -e " Path WebSocket     : /brody${off}"
-echo -e "${red}=================================${off}"
-echo -e " Trojan-GO   : ${trojangolink}" | lolcat
-echo -e "${red}=================================${off}"
-echo -e " Igniter-GO  : http://${domain}:81/${user}-IgniterGO.json" | lolcat
-echo -e "${red}=================================${off}"
-echo -e " ${white}Aktif Selama   : $masaaktif Hari"
-echo -e " Dibuat Pada    : $tnggl"
-echo -e " Berakhir Pada  : $expe${off}"
-echo -e "${red}=================================${off}"
+echo -e "=======-TROJAN-GO-======="
+echo -e "Remarks    : ${user}"
+echo -e "IP/Host    : ${MYIP}"
+echo -e "Address    : ${domain}"
+echo -e "Port       : ${trgo}"
+echo -e "Key        : ${user}"
+echo -e "Encryption : none"
+echo -e "Path       : /trojango"
+echo -e "Created    : $hariini"
+echo -e "Expired    : $exp"
+echo -e "========================="
+echo -e "Link TrGo  : ${link}"
+echo -e "========================="
+echo -e "Script By GEO GABUT"
